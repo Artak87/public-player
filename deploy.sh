c0377a342d7660c33fc32cb4887fc702540b2d2e
Ashot Mnatsakanyan <ashot.mnatsakanyan@ideus.co>
2016-12-29 12:19:09 +0400

    Improved ci commands 2

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-DOCKER_CERT_PATH=~/.docker/cloud_hero
+DOCKER_CERT_PATH=.cloud_hero
+echo $DOCKER_CERT_PATH
 DOCKER_TLS_VERIFY=1
+echo $DOCKER_TLS_VERIFY
 DOCKER_HOST='tcp://swarm3541.chapi.co:8443'
+echo $DOCKER_HOST
 mkdir -p $DOCKER_CERT_PATH
-CLOUDHERO_PROFILE=~/.cloudhero_profile
+ls -la
+CLOUDHERO_PROFILE=.cloudhero_profile
 
 echo export DOCKER_CERT_PATH=$DOCKER_CERT_PATH > $CLOUDHERO_PROFILE
 echo export DOCKER_HOST=$DOCKER_HOST >> $CLOUDHERO_PROFILE
