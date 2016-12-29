68238c3139a1261f9db3252a2d9e1cc2b3e297bc
Ashot Mnatsakanyan <ashot.mnatsakanyan@ideus.co>
2016-12-29 12:14:54 +0400

    Improved ci commands 2

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+DOCKER_CERT_PATH=~/.docker/cloud_hero
+DOCKER_TLS_VERIFY=1
+DOCKER_HOST='tcp://swarm3541.chapi.co:8443'
+mkdir -p $DOCKER_CERT_PATH
+CLOUDHERO_PROFILE=~/.cloudhero_profile
+
+echo export DOCKER_CERT_PATH=$DOCKER_CERT_PATH > $CLOUDHERO_PROFILE
+echo export DOCKER_HOST=$DOCKER_HOST >> $CLOUDHERO_PROFILE
+echo export DOCKER_TLS_VERIFY=1 >> $CLOUDHERO_PROFILE
+
+curl -s https://portal.cloudhero.io/bootstrap/CT8120WX6J1X3WW6HC1K58815N8FABEG/ca > $DOCKER_CERT_PATH/ca.pem
+curl -s https://portal.cloudhero.io/bootstrap/CT8120WX6J1X3WW6HC1K58815N8FABEG/cert > $DOCKER_CERT_PATH/cert.pem
+curl -s https://portal.cloudhero.io/bootstrap/CT8120WX6J1X3WW6HC1K58815N8FABEG/key > $DOCKER_CERT_PATH/key.pem
+
+cat $CLOUDHERO_PROFILE
+echo
+echo '#You can also set the environment by running:'
+echo '#source ' $CLOUDHERO_PROFILE
+echo
+echo '#Run our demo container (and access it through a browser at at http://swarm3541.chapi.co):'
+echo '#docker run -d -p 80:80 cloudhero/apache-php'
